02e67e6ce1205d986e21731e610cceeb3189f3c5
이남곤 <nglee.dev@gmail.com>
2023-09-05 15:45:17 +0900

    [BE/init] global 속성들 설정

--- backend/haruman/src/main/java/ssafy/haruman/HarumanApplication.java
+++ backend/haruman/src/main/java/ssafy/haruman/HarumanApplication.java
@@ -2,8 +2,10 @@ package ssafy.haruman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HarumanApplication {
 
     public static void main(String[] args) {
