165beff182ec0b8038a0c785f093efad4511330a
gobro <gobro6366@gmail.com>
2023-09-05 12:52:57 +0900

    [FE/init] 프론트엔드 초기 세팅   - 각 페이지 라우팅   - 로그인 관련 redux 구축(임시 주석 처리)   - isLogin True / False 여부에 따라 랜딩페이지로 (임시 주석 처리)   - 그에 해당하는 것은 store/escrypt, app.tsx의 주석 부분 , store의 주석 부분   - store의 userSclice는 견본, 추가로 api 명세서 나오면 그에 맞게 수정

---
+++ frontend/src/pages/CalendarPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <>
+      <h1>CALENDAR 및 세부 내역이 있는 페이지 입니다.</h1>
+    </>
+  );
+};
+
+export default Homepage;
---
+++ frontend/src/pages/SignUpPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUpPage = () => {
+  return (
+    <>
+      <h1>SIGNUP PAGE입니다.</h1>
+    </>
+  );
+};
+
+export default SignUpPage;
