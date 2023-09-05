f498c4d8948b0af340178ec23792539d51f2bb4b
이남곤 <nglee.dev@gmail.com>
2023-09-05 15:52:38 +0900

    [BE/feat] Profile Entity 생성

--- backend/haruman/src/main/java/ssafy/haruman/domain/profile/entity/Profile.java
+++ backend/haruman/src/main/java/ssafy/haruman/domain/profile/entity/Profile.java
@@ -1,4 +1,26 @@
 package ssafy.haruman.domain.profile.entity;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "profile_id")
+    private Long id;
+
+    private String nickname;
+
+    @Builder
+    public Profile(String nickname) {
+        this.nickname = nickname;
+    }
 }
