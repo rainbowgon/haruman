fec7b94e5c974616ff087f29c5e54f63a2910e33
고세훈 <gobro6366@gmail.com>
2023-09-05 12:57:46 +0900

    [FE/init] 프론트엔드 초기 세팅

---
+++ frontend/README.md
@@ -0,0 +1,6 @@
+- 23.09.04 KSH
+  - 각 페이지 라우팅
+  - 로그인 관련 redux 구축(임시 주석 처리)
+  - isLogin True / False 여부에 따라 랜딩페이지로 (임시 주석 처리)
+  - 그에 해당하는 것은 store/escrypt, app.tsx의 주석 부분 , store의 주석 부분
+  - store의 userSclice는 견본, 추가로 api 명세서 나오면 그에 맞게 수정
---
+++ frontend/src/App.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
+
+// pages
+import HomePage from "./pages/HomePage";
+import LandingPage from "./pages/LandingPage";
+import SignUpPage from "./pages/SignUpPage";
+import MyPage from "./pages/MyPage";
+
+// redux
+import { useAppSelector } from "./hooks/reduxHook";
+
+// react-router-dom
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const AppContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  overflow-x: hidden;
+  // 가로 이동 없을거니까
+`;
+
+const App: React.FC = () => {
+  // const isLogin = useAppSelector((state) => state.user.isLogin);
+
+  return (
+    <>
+      <AppContainer>
+        <Router>
+          <Routes>
+            {/*   로그인 및 회원가입 기능 이후에 로그인 처리 안되면 랜딩페이지로
+          <Route path="/" element={isLogin ? <LandingPage /> : <LandingPage />} />
+          <Route path="/signup" element={isLogin ? <SignUpPage /> : <SignUpPage />} />
+          <Route path="/home" element={isLogin ? <HomePage /> : <LandingPage />} />
+          <Route path="/mypage" element={isLogin ? <MyPage /> : <LandingPage />} />
+          */}
+
+            <Route
+              path="/"
+              element={<LandingPage />}
+            />
+            <Route
+              path="/home"
+              element={<HomePage />}
+            />
+            <Route
+              path="/signup"
+              element={<SignUpPage />}
+            />
+            <Route
+              path="/mypage"
+              element={<MyPage />}
+            />
+          </Routes>
+        </Router>
+      </AppContainer>
+    </>
+  );
+};
+
+export default App;
