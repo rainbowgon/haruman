d7f6b05eae03aaebf702a64e7a4fb01709cd1ca2
최해미 <goal961@naver.com>
2023-09-08 16:26:38 +0900

    [BE/init] Category 도메인 초기 설정

---
+++ backend/src/main/java/ssafy/haruman/domain/category/service/CategoryServiceImpl.java
@@ -0,0 +1,12 @@
+package ssafy.haruman.domain.category.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+}
