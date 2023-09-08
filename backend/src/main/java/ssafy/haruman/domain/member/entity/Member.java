f9bd954e6f72ac7c0c7feb9d18496b37d24ea904
이남곤 <nglee.dev@gmail.com>
2023-09-08 14:40:59 +0900

    [BE/feat] ãMember entity 생성

--- backend/src/main/java/ssafy/haruman/domain/member/entity/Member.java
+++ backend/src/main/java/ssafy/haruman/domain/member/entity/Member.java
@@ -1,4 +1,16 @@
 package ssafy.haruman.domain.member.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private UUID id;
 }
