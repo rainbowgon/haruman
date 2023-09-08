06cf88380880eaa3f43ecbdbc8f2b00b230ed2ae
이남곤 <nglee.dev@gmail.com>
2023-09-08 16:31:55 +0900

    [BE/feat]  ProfileImage 생성

--- backend/src/main/java/ssafy/haruman/domain/profile/entity/ProfileImage.java
+++ backend/src/main/java/ssafy/haruman/domain/profile/entity/ProfileImage.java
@@ -1,2 +1,28 @@
-package ssafy.haruman.domain.profile.entity;public class ProfileImage {
+package ssafy.haruman.domain.profile.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import ssafy.haruman.global.entity.File;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToOne;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DiscriminatorValue(value = "ProfileImage")
+public class ProfileImage extends File {
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Profile profile;
+
+    @Builder
+    public ProfileImage(String savedFilename, String savedPath, Profile profile) {
+        super(savedFilename, savedPath);
+        this.profile = profile;
+    }
 }
