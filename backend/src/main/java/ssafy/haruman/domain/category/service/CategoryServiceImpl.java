8765105740b39cc3f94cf33c62c9272d91e59a45
최해미 <goal961@naver.com>
2023-09-08 17:25:11 +0900

    [BE/fix] 카테고리 Service @Transactional 어노테이션 수정

--- backend/src/main/java/ssafy/haruman/domain/category/service/CategoryServiceImpl.java
+++ backend/src/main/java/ssafy/haruman/domain/category/service/CategoryServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
 
