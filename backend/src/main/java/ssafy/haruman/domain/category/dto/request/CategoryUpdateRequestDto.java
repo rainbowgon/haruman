583b8034d437809c69c20c86ff054ef63773a3d2
최해미 <goal961@naver.com>
2023-09-08 16:50:33 +0900

    [BE/fix] 카테고리 RequestDto 어노테이션 수정

--- backend/src/main/java/ssafy/haruman/domain/category/dto/request/CategoryUpdateRequestDto.java
+++ backend/src/main/java/ssafy/haruman/domain/category/dto/request/CategoryUpdateRequestDto.java
@@ -1,15 +1,11 @@
 package ssafy.haruman.domain.category.dto.request;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
 public class CategoryUpdateRequestDto {
 
 }
