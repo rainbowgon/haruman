81877ffa9dfaa6593b966beb5c882fed6bd92e6f
이남곤 <nglee.dev@gmail.com>
2023-09-08 17:09:17 +0900

    [BE/feat] ProfileCreateRequestDto 생성

--- backend/src/main/java/ssafy/haruman/domain/profile/dto/request/ProfileCreateRequestDto.java
+++ backend/src/main/java/ssafy/haruman/domain/profile/dto/request/ProfileCreateRequestDto.java
@@ -1,4 +1,18 @@
 package ssafy.haruman.domain.profile.dto.request;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import ssafy.haruman.domain.profile.entity.Profile;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProfileCreateRequestDto {
+
+    private String nickname;
+
+    public Profile toEntity() {
+        return Profile.builder()
+                .nickname(nickname).build();
+    }
 }
