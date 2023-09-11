c6294dad1718178dd2fba9415f195cd6f7861618
gobro <gobro6366@gmail.com>
2023-09-11 15:04:39 +0900

    [FE/style] 로그인 페이지 색상 및 레이아웃 수정

--- frontend/src/components/CheckBox.tsx
+++ frontend/src/components/CheckBox.tsx
@@ -1,28 +1,64 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import "../styles/theme.css";
 
-interface CheckboxProps {
+// interface CheckboxProps {
+//   label: string;
+//   checked: boolean;
+//   onChange: (isChecked: boolean) => void;
+// }
+
+// const Checkbox: React.FC<CheckboxProps> = ({ label }) => {
+//   const [checked, setChecked] = useState(false);
+
+//   const handleChange = () => {
+//     setChecked(!checked);
+//   };
+
+//   return (
+//     <div>
+//       <label>
+//         <input
+//           type="checkbox"
+//           checked={checked}
+//           onChange={handleChange}
+//         />
+//         {label}
+//       </label>
+//     </div>
+//   );
+// };
+
+// export default Checkbox;
+
+import React from "react";
+import styled from "styled-components";
+import "../styles/theme.css";
+
+export interface CheckboxProps {
   label: string;
+  checked: boolean;
+  onChange: (isChecked: boolean) => void;
 }
 
-const Checkbox: React.FC<CheckboxProps> = ({ label }) => {
-  const [checked, setChecked] = useState(false);
+const CheckBoxButton = styled.input`
+  border: 2px solid var(--brand1_main);
+  color: var(--white);
+  background-color: var(--brand1_main);
+`;
 
+export default function Checkbox({ label, checked, onChange }: CheckboxProps) {
   const handleChange = () => {
-    setChecked(!checked);
+    onChange(!checked);
   };
 
   return (
-    <div>
-      <label>
-        <input
-          type="checkbox"
-          checked={checked}
-          onChange={handleChange}
-        />
-        {label}
-      </label>
-    </div>
+    <label>
+      <CheckBoxButton
+        type="checkbox"
+        checked={checked}
+        onChange={handleChange}
+      />
+      {label}
+    </label>
   );
-};
-
-export default Checkbox;
+}
