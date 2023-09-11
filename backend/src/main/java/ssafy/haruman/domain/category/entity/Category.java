6118d7177509f8666edd9bffde6ade870b8dbe74
최해미 <goal961@naver.com>
2023-09-11 13:56:37 +0900

    [BE/fix] Category id 타입 unsigned int -> int unsigned

--- backend/src/main/java/ssafy/haruman/domain/category/entity/Category.java
+++ backend/src/main/java/ssafy/haruman/domain/category/entity/Category.java
@@ -24,7 +24,7 @@ public class Category extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "category_id", columnDefinition = "UNSIGNED INT")
+    @Column(name = "category_id", columnDefinition = "INT UNSIGNED")
     private Long id;
 
     @ManyToOne(optional = true, fetch = FetchType.LAZY)
