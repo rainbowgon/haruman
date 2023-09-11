9172a36731f64c18e0985afe50dc518e16387fad
전혜련 <davidpoplar0531@gmail.com>
2023-09-11 15:25:37 +0900

    [ãBE/feat] 챌린지 엔티티 생성

--- backend/src/main/java/ssafy/haruman/domain/challenge/dto/response/ChallengeResponseDto.java
+++ backend/src/main/java/ssafy/haruman/domain/challenge/dto/response/ChallengeResponseDto.java
@@ -1,15 +1,14 @@
 package ssafy.haruman.domain.challenge.dto.response;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@AllArgsConstructor
 public class ChallengeResponseDto {
 
 }
