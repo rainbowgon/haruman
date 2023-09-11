2d85225e08c57fcd3655d0cc4eaa3634d3204b8a
leejm9704 <leejm9704@naver.com>
2023-09-11 16:46:26 +0900

    [FE/save] Homepage 링크 추가 및 개발 예정 페이지 추가
    - HomePage 링크 추가
    - NotFound, TodayExpense 페이지 추가

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,46 +2,46 @@ import React from "react";
 import MainStyle from "../components/MainStyle";
 import "../styles/theme.css";
 import "../styles/wave.css";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
     <MainStyle>
       <h1>HOMEPAGE (메인 동그라미 있고 한 페이지) 입니다.</h1>
-      <div className="header">
       <div>
-        <div className="hei">
+        <div className="header">
+          <div>
+            <div className="hei"/>
+            <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+              <defs>
+                <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+              </defs>
+              <g className="parallax">
+                <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(93,215,190,0.7)" />
+                <use xlinkHref="#gentle-wave" x="48" y="5" fill="var(--brand2_op_40)" />
+                <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(93,215,190,0.3)" />
+                <use xlinkHref="#gentle-wave" x="48" y="5" fill="var(--gradation-start)" />
+              </g>
+            </svg>
+          </div>
+          <div className="color"/>
         </div>
-        <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
-        <defs>
-          <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-        </defs>
-        <g className="parallax">
-          <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(93,215,190,0.7)" />
-          <use xlinkHref="#gentle-wave" x="48" y="5" fill="var(--brand2_op_40)" />
-          <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(93,215,190,0.3)" />
-          <use xlinkHref="#gentle-wave" x="48" y="5" fill="var(--gradation-start)" />
-        </g>
-      </svg>
+        <Link to="/today">today로 이동 </Link>
       </div>
-      <div className="color">
-      </div>
-      </div>
-        <div className="getspace">
-        </div>
+        <div className="getspace"/>
         <div>
           <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
             <defs>
               <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
             </defs>
             <g className="parallax">
-            <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(93,215,190,0.7" />
-            <use xlinkHref="#gentle-wave" x="48" y="3" fill="var(--brand2_op_40)" />
-            <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(93,215,190,0.3)" />
-            <use xlinkHref="#gentle-wave" x="48" y="8" fill="var(--gradation-start)" />
+              <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(93,215,190,0.7" />
+              <use xlinkHref="#gentle-wave" x="48" y="3" fill="var(--brand2_op_40)" />
+              <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(93,215,190,0.3)" />
+              <use xlinkHref="#gentle-wave" x="48" y="8" fill="var(--gradation-start)" />
             </g>
           </svg>
-        <div className="color">
-        </div>
+        <div className="color"/>
       </div>
     </MainStyle>
   );
