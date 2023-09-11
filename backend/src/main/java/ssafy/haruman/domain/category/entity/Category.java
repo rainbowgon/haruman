c5f2c22ff7ed1755557dca24af8a521cb68ce8cf
최해미 <goal961@naver.com>
2023-09-11 13:46:56 +0900

    [BE/feat] Category entity 생성

--- backend/src/main/java/ssafy/haruman/domain/category/entity/Category.java
+++ backend/src/main/java/ssafy/haruman/domain/category/entity/Category.java
@@ -2,12 +2,19 @@ package ssafy.haruman.domain.category.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import ssafy.haruman.domain.profile.entity.Profile;
 import ssafy.haruman.global.entity.BaseEntity;
 
 @Entity
@@ -19,5 +26,17 @@ public class Category extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "category_id", columnDefinition = "UNSIGNED INT")
     private Long id;
+
+    @ManyToOne(optional = true, fetch = FetchType.LAZY)
+    private Profile profile;
+
+    @Column(length = 20)
+    @NotBlank
+    private String name;
+
+    @Column(length = 10)
+    @Enumerated(value = EnumType.STRING)
+    @NotNull
+    private IsDefault isDefault;
 
 }
