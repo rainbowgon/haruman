af83e0c698c98f42f7043234dad1e17eff799615
전혜련 <davidpoplar@naver.com>
2023-09-11 15:44:28 +0900

    [BE/init] 챌린지 초기 환경설정

---
+++ backend/src/main/java/ssafy/haruman/domain/challenge/dto/response/ChallengeResponseDto.java
@@ -0,0 +1,14 @@
+package ssafy.haruman.domain.challenge.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@AllArgsConstructor
+public class ChallengeResponseDto {
+
+}
---
+++ backend/src/main/java/ssafy/haruman/domain/challenge/service/ChallengeServiceImpl.java
@@ -0,0 +1,10 @@
+package ssafy.haruman.domain.challenge.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChallengeServiceImpl implements ChallengeService {
+
+}
