0f775aa7faf3e85ad6c4731cfe3ccd0bdcb1aee1
고세훈 <gobro6366@gmail.com>
2023-09-11 16:55:54 +0900

    [FE/feat] Signup 페이지 구현

--- frontend/src/components/CheckBox.tsx
+++ frontend/src/components/CheckBox.tsx
@@ -1,64 +1,85 @@
-// import React, { useState } from "react";
+// import React from "react";
+// import styled from "styled-components";
 // import "../styles/theme.css";
 
-// interface CheckboxProps {
+// export interface CheckboxProps {
 //   label: string;
 //   checked: boolean;
 //   onChange: (isChecked: boolean) => void;
 // }
 
-// const Checkbox: React.FC<CheckboxProps> = ({ label }) => {
-//   const [checked, setChecked] = useState(false);
+// const CheckBoxButton = styled.input`
+//   border: 2px solid var(--brand1_main);
+//   color: var(--white);
+//   background-color: var(--brand1_main);
+// `;
 
+// export default function Checkbox({ label, checked, onChange }: CheckboxProps) {
 //   const handleChange = () => {
-//     setChecked(!checked);
+//     onChange(!checked);
 //   };
 
 //   return (
-//     <div>
-//       <label>
-//         <input
-//           type="checkbox"
-//           checked={checked}
-//           onChange={handleChange}
-//         />
-//         {label}
-//       </label>
-//     </div>
+//     <label>
+//       <CheckBoxButton
+//         type="checkbox"
+//         checked={checked}
+//         onChange={handleChange}
+//       />
+//       {label}
+//     </label>
 //   );
-// };
+// }
 
-// export default Checkbox;
+import React, { useContext } from "react";
 
-import React from "react";
 import styled from "styled-components";
-import "../styles/theme.css";
 
-export interface CheckboxProps {
+interface CheckboxProps {
   label: string;
   checked: boolean;
-  onChange: (isChecked: boolean) => void;
+  onChange: (event: any) => void;
 }
 
-const CheckBoxButton = styled.input`
-  border: 2px solid var(--brand1_main);
-  color: var(--white);
-  background-color: var(--brand1_main);
+const CheckboxContainer = styled.label`
+  display: flex;
+  justify-content: center;
+  cursor: pointer;
 `;
 
-export default function Checkbox({ label, checked, onChange }: CheckboxProps) {
-  const handleChange = () => {
-    onChange(!checked);
-  };
+const CheckboxInput = styled.input`
+  appearance: none;
+  width: 18px;
+  height: 18px;
+  border-radius: 4px;
+  margin: 4px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  border: 1px solid var(--brand2_main);
+  background-color: ${(props) => (props.checked ? "var(--brand2_main)" : "")};
+  color: var(--brand2_main);
 
+  &:checked {
+    background: var(--brand2_main);
+    transition: all 0.1s ease-in-out;
+  }
+`;
+
+const CheckboxLabel = styled.span`
+  font-size: 20px;
+  margin: 4px;
+  margin-bottom: 12px;
+`;
+
+export default function CheckBox({ label, checked, onChange }: CheckboxProps) {
   return (
-    <label>
-      <CheckBoxButton
+    <CheckboxContainer>
+      <CheckboxInput
         type="checkbox"
         checked={checked}
-        onChange={handleChange}
+        onChange={onChange}
       />
-      {label}
-    </label>
+      <CheckboxLabel>{label}</CheckboxLabel>
+    </CheckboxContainer>
   );
 }
