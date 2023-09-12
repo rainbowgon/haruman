f842dbc2f6bd1b0e3dba889407eb112f5ddd1b37
leejm9704 <leejm9704@naver.com>
2023-09-12 17:57:12 +0900

    [FE/save] CalenderFrom 저장
    - CalenderFrom 생성 및 디자인 기초

--- frontend/src/pages/CalendarPage.tsx
+++ frontend/src/pages/CalendarPage.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import CalendarForm from "../components/Calender/CalenderForm";
+import MainStyle from "../components/MainStyle";
 
 const CalendarPage = () => {
   return (
-    <>
-      <h1>CALENDAR 및 세부 내역이 있는 페이지 입니다.</h1>
-    </>
+    <MainStyle>
+      <div className="calendar_header">
+        <h2 className="sub_title">지금까지 모은금액!</h2>
+        <h1 className="main_title">{} 원</h1>
+      </div>
+      <div className="calendar_form">
+        <CalendarForm />
+      </div>
+      <div>
+        {/* 선택 달 성공 실패 횟수 */}
+      </div>
+      <div>
+        {/* 결제한 아이템 리스트 */}
+      </div>
+    </MainStyle>
   );
 };
 
