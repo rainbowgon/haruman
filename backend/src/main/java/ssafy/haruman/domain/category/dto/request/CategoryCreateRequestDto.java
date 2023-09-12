6ad1bf6aa540c6bfdd45a29f9c1db043b84a323b
최해미 <goal961@naver.com>
2023-09-12 17:12:48 +0900

    [BE/feat] Category service CRUD 로직 구현

--- backend/src/main/java/ssafy/haruman/domain/category/dto/request/CategoryCreateRequestDto.java
+++ backend/src/main/java/ssafy/haruman/domain/category/dto/request/CategoryCreateRequestDto.java
@@ -3,11 +3,22 @@ package ssafy.haruman.domain.category.dto.request;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import ssafy.haruman.domain.category.entity.Category;
+import ssafy.haruman.domain.category.entity.CustomStatus;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CategoryCreateRequestDto {
 
     private String name;
+
+    public Category toEntity() {
+        return Category.builder()
+                // TODO 회원 프로필 추가
+                .name(name)
+                .isDefault(CustomStatus.CUSTOM)
+                .build();
+    }
+
 
 }
