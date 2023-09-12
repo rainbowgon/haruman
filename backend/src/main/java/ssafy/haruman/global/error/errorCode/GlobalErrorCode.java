ee9069da40170da2290b332a7a727309b4cfb77e
이남곤 <nglee.dev@gmail.com>
2023-09-12 15:18:34 +0900

    [BE/chore] 구분을 위한 주석을 추가

--- backend/src/main/java/ssafy/haruman/global/error/errorCode/GlobalErrorCode.java
+++ backend/src/main/java/ssafy/haruman/global/error/errorCode/GlobalErrorCode.java
@@ -12,10 +12,14 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 @AllArgsConstructor
 public enum GlobalErrorCode implements BaseErrorCode {
 
+    /**
+     * 예시
+     */
     EXAMPLE_NOT_FOUND(NOT_FOUND, "EXAMPLE_404_1", "예시를 찾을 수 없는 오류입니다."),
 
     CUSTOM_INTERNAL_SERVER_ERROR(INTERNAL_SERVER_ERROR, "GLOBAL_500_1", "서버 오류. 관리자에게 문의 부탁드립니다."),
 
+    /* 카테고리 */
     CATEGORY_NOT_FOUND(NOT_FOUND, "CATEGRORY_404_1", "해당 ID의 카테고리가 없습니다.");
 
     private HttpStatus status;
