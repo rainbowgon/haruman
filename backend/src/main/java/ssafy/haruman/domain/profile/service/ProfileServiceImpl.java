e4fa11a7f3d1189844618c56f350ecbcd7392d3c
이남곤 <nglee.dev@gmail.com>
2023-09-12 13:22:18 +0900

    [BE/feat] Service에서 s3 업로드를 진행하고 ProfileImage 객체 생성

--- backend/src/main/java/ssafy/haruman/domain/profile/service/ProfileServiceImpl.java
+++ backend/src/main/java/ssafy/haruman/domain/profile/service/ProfileServiceImpl.java
@@ -10,6 +10,8 @@ import ssafy.haruman.domain.profile.entity.Profile;
 import ssafy.haruman.domain.profile.repository.ProfileRepository;
 import ssafy.haruman.global.service.S3FileService;
 
+import java.io.IOException;
+
 @Service
 @RequiredArgsConstructor
 public class ProfileServiceImpl implements ProfileService {
@@ -17,12 +19,20 @@ public class ProfileServiceImpl implements ProfileService {
     private final ProfileRepository profileRepository;
     private final S3FileService s3FileService;
 
+    private final String S3_PATH = "image/profile/";
+
     @Override
     public SingleProfileResponseDto createProfile(ProfileCreateRequestDto profileCreateRequestDto, MultipartFile multipartFile) {
         Profile profile = profileCreateRequestDto.toEntity();
-        // TODO S3에 이미지 저장 및 URL 반환
-        // TODO ProfileImage 개체를 만들고 Profile에 넣어야 합니다.
-        return SingleProfileResponseDto.from(profile, null);
+
+        // TODO 이후 예외 처리 방식이 결정된다면 try~catch 문 대신 throw Exception 하는 방식으로 수정해야 합니다.
+        try {
+            String savedFilename = s3FileService.saveFile(S3_PATH, multipartFile);
+            profile.uploadNewProfileImage(S3_PATH, savedFilename);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return SingleProfileResponseDto.from(profile, s3FileService.getS3Url(profile.getProfileImage()));
     }
 
     @Override
