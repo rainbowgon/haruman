d9fab905a3c5095a99609ff948f66667f52853b1
전혜련 <davidpoplar0531@gmail.com>
2023-09-12 13:12:36 +0900

    [BE/feat] 챌린지 컨트롤러 작성

--- backend/src/main/java/ssafy/haruman/domain/challenge/controller/ChallengeController.java
+++ backend/src/main/java/ssafy/haruman/domain/challenge/controller/ChallengeController.java
@@ -1,12 +1,67 @@
 package ssafy.haruman.domain.challenge.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ssafy.haruman.domain.challenge.dto.request.ExpenseCreateRequestDto;
+import ssafy.haruman.domain.challenge.dto.request.ExpenseUpdateRequestDto;
+import ssafy.haruman.domain.challenge.dto.response.ChallengeResponseDto;
+import ssafy.haruman.domain.challenge.service.ChallengeService;
+import ssafy.haruman.global.response.JsonResponse;
+import ssafy.haruman.global.response.ResponseWrapper;
 
 @RestController
 @RequestMapping("/challenges")
 @RequiredArgsConstructor
 public class ChallengeController {
+
+    private final ChallengeService challengeService;
+
+    @PostMapping
+    public ResponseEntity<ResponseWrapper<ChallengeResponseDto>> startChallenge() {
+
+        return JsonResponse.ok("챌린지가 생성되었습니다.", null);
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseWrapper<ChallengeResponseDto>> createExpense(
+            @PathVariable(name = "challenge-id") Long chellengeId,
+            @RequestBody ExpenseCreateRequestDto createDto) {
+
+        return JsonResponse.ok("지출내역이 입력되었습니다.", null);
+    }
+
+    @PatchMapping
+    public ResponseEntity<ResponseWrapper<ChallengeResponseDto>> updateExpense(
+            @PathVariable(name = "challenge-id") Long chellengeId,
+            @RequestBody ExpenseUpdateRequestDto updateDto) {
+
+        return JsonResponse.ok("지출내역이 수정되었습니다.", null);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<ResponseWrapper<Nullable>> deleteExpense(
+            @PathVariable(name = "challenge-id") Long chellengeId) {
+
+        return JsonResponse.ok("지출내역이 삭제되었습니다.", null);
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapper<List<ChallengeResponseDto>>> selectDailyChallenge(
+            @PathVariable(name = "date") LocalDateTime date) {
+
+        return JsonResponse.ok("챌린지 상세내역을 불러왔습니다.", null);
+    }
+
 
 }
--- backend/src/main/java/ssafy/haruman/domain/challenge/dto/request/ExpenseCreateRequestDto.java
+++ backend/src/main/java/ssafy/haruman/domain/challenge/dto/request/ExpenseCreateRequestDto.java
@@ -8,4 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ExpenseCreateRequestDto {
 
+    private String category;
+    private int payAmount;
+    private String content;
+
 }
