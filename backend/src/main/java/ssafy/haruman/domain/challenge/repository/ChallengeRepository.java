4ce0e2a8168ff069a1abc4f7b02985e7cb33a205
최해미 <goal961@naver.com>
2023-09-13 17:28:52 +0900

    [BE/feat] Challenge 통계(현황판) 기능 controller, repository 구현

--- backend/src/main/java/ssafy/haruman/domain/challenge/repository/ChallengeRepository.java
+++ backend/src/main/java/ssafy/haruman/domain/challenge/repository/ChallengeRepository.java
@@ -1,8 +1,18 @@
 package ssafy.haruman.domain.challenge.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ssafy.haruman.domain.challenge.entity.Challenge;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 
+    @Query(nativeQuery = true,
+            value = "SELECT c.challenge_id, c.profile_id, c.start_time, c.used_amount, MAX(e.created_at)"
+                    + "FROM challenge c LEFT JOIN expense e"
+                    + "ON c.challenge_id = e.challenge_id"
+                    + "WHERE c.challenge_status = 'PROGRESS'"
+                    + "GROUP BY c.challenge_id"
+                    + "ORDER BY e.created_at, c.start_time")
+    List<ChallengeUserListMapping> findChallengesByStatus();
 }
--- backend/src/main/java/ssafy/haruman/domain/challenge/service/ChallengeService.java
+++ backend/src/main/java/ssafy/haruman/domain/challenge/service/ChallengeService.java
@@ -1,9 +1,13 @@
 package ssafy.haruman.domain.challenge.service;
 
+import java.util.List;
 import ssafy.haruman.domain.challenge.dto.response.ChallengeResponseDto;
+import ssafy.haruman.domain.challenge.dto.response.ChallengeUserListResponseDto;
 import ssafy.haruman.domain.profile.entity.Profile;
 
 public interface ChallengeService {
 
     ChallengeResponseDto startChallenge(Profile profile);
+
+    List<ChallengeUserListResponseDto> selectDailyUserList();
 }
