3a455a328ea1b5af541255eefd8c9c06cd68406d
leejm9704 <leejm9704@naver.com>
2023-09-13 17:08:18 +0900

    [FE/desgin] 에러페이지 디자인
    - 에러페이지 디자인 및 데이터 입력 시, 처리를 위한 변수 설정

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import "../styles/NotFoundPageStyle.scss"
 
 const NotFoundPage = () => {
+  const [errorLog, setErrorLog] = useState("404");
+  
+  useEffect(() => {
+    
+  }, [])
+
   return (
     <>
-        <h1>404 not Found</h1>
-        {/* <h1> {notfound} not Found</h1> */}
+      <div className="notfount">
+        <h1 className="notfound_title">{errorLog} ERROR</h1>
+        <p className="notfound_text">이 페이지는 아직 공사중이에요!</p>
+      </div>
     </>
   );
 };
