5afcae263b36b6ac88a6dee16fafd9a7d4c5a6f2
이남곤 <nglee.dev@gmail.com>
2023-09-13 17:49:24 +0900

    [BE/fix] Profile DELETE 수정

--- backend/src/main/java/ssafy/haruman/domain/profile/controller/ProfileController.java
+++ backend/src/main/java/ssafy/haruman/domain/profile/controller/ProfileController.java
@@ -3,7 +3,6 @@ package ssafy.haruman.domain.profile.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ssafy.haruman.domain.profile.dto.response.SingleProfileResponseDto;
@@ -42,12 +41,5 @@ public class ProfileController {
             @PathVariable("profile-id") Long profileId) {
         SingleProfileResponseDto singleProfileResponseDto = profileService.selectOneProfile(profileId);
         return JsonResponse.ok("프로필을 성공적으로 가져왔습니다.", singleProfileResponseDto);
-    }
-
-    @DeleteMapping("/{profile-id}")
-    public ResponseEntity<ResponseWrapper<Nullable>> deleteProfile(
-            @PathVariable("profile-id") Long profileId) {
-        profileService.deleteProfile(profileId);
-        return JsonResponse.of(HttpStatus.NO_CONTENT, "프로필을 성공적으로 삭제했습니다.");
     }
 }
