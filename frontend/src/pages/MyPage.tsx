e019c9115980f8e0cb32eae3b1e91e552804a89a
leejm9704 <leejm9704@naver.com>
2023-09-18 17:41:24 +0900

    [FE/feat] myPage Button 기본 버튼 추가
    - 디자인 X
    - 버튼 출력을 위한 레이아웃 추가

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,13 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
 import CenterContainer from "../components/CenterContainer";
 import MainStyle from "../components/MainStyle";
 import ProfileImageUploader from "../components/ProfileImage";
+import RegisterButton from "../components/RegistButton";
 
 const MyPage = () => {
+
+  const [features, setFeatures] = useState(["HomePage", "TodayExpensePage", "CalendarPage", "RankingPage", "plus"]);
+  
+  const handleEditProfile = () => {
+
+  }
+
+  const handleEditCategory = () => {
+
+  }
+
   return (
     <CenterContainer>
       <MainStyle>
         <ProfileImageUploader />
+        <div>
+          <p>하루만과 함께한지 {}일이 지났네요</p>
+        </div>
+        <div>
+          <div>
+            <RegisterButton
+              text="프로필 수정"
+              className="regular_type brand"
+              onClick={handleEditProfile}
+            />
+            <RegisterButton
+              text="카테고리 수정"
+              className="regular_type"
+              onClick={handleEditCategory}
+            />
+          </div>
+        </div>
+        <div>
+          { features.map((feature) => (
+            <RegisterButton
+              text={feature}
+              className="regular_type"  
+              onClick={handleEditCategory}
+            />
+          ))}
+        </div>
+        <div>
+          <RegisterButton
+            text="로그아웃"
+            className="regular_type highlight"
+            onClick={handleEditCategory}
+          />
+        </div>
       </MainStyle>
     </CenterContainer>
   );
