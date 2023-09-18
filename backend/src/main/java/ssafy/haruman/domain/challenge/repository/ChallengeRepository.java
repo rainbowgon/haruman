7dd5b29ec0703f3326c27bbc8726efa5953c1c9a
chogoal <goal961@naver.com>
2023-09-19 02:00:00 +0900

    [BE/feat] 현재 챌린지 중인 유저 목록 조회 쿼리 완성

--- backend/src/main/java/ssafy/haruman/domain/challenge/repository/ChallengeRepository.java
+++ backend/src/main/java/ssafy/haruman/domain/challenge/repository/ChallengeRepository.java
@@ -8,11 +8,14 @@ import ssafy.haruman.domain.challenge.entity.Challenge;
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 
     @Query(nativeQuery = true,
-            value = "SELECT c.challenge_id, c.profile_id, c.start_time, c.used_amount, MAX(e.created_at)"
-                    + "FROM challenge c LEFT JOIN expense e"
-                    + "ON c.challenge_id = e.challenge_id"
-                    + "WHERE c.challenge_status = 'PROGRESS'"
-                    + "GROUP BY c.challenge_id"
+            value = "SELECT p.nickname, f.saved_path, c.start_time, c.used_amount, MAX(e.created_at)\n"
+                    + "FROM challenge c\n"
+                    + "JOIN expense e USING (challenge_id)\n"
+                    + "JOIN profile p USING (profile_id)\n"
+                    + "LEFT JOIN profile_image i USING (profile_id)\n"
+                    + "LEFT JOIN file f ON (f.file_id = i.file_id)\n"
+                    + "WHERE c.challenge_status = 'PROGRESS'\n"
+                    + "GROUP BY e.challenge_id\n"
                     + "ORDER BY e.created_at, c.start_time")
     List<ChallengeUserInfoMapping> findChallengesByStatus();
 }
