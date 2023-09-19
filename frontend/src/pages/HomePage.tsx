19943b4c0a3b686650e4d8f5061b07b6539c10fa
이정명 <leejm9704@naver.com>
2023-09-19 17:44:11 +0900

    [FE/feat] 메인/로그인/회원가입 페이지 디자인

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -4,14 +4,25 @@ import "../styles/theme.css";
 import "../styles/wave.scss";
 import { Link } from "react-router-dom";
 
+// components
+import InputText from "../components/InputText";
+import RegisterButton from "../components/RegistButton";
+
 const Homepage = () => {
+  const baseURL = 'https://i9a608.p.ssafy.io:8000';
+  const ChallengeAPI = '/challenges';
+  const CategorieAPI = '/categories';
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [challenge, setChallenge] = useState(true);
   const [height, setHeight] = useState(100);
 
   const handleStartChallenge = () => {
     const nowChallenge = !challenge;
   
+    //챌린지 시작 
     setChallenge(nowChallenge);
+
+    startChallenge();
   
     let targetHeight: number = nowChallenge ? height + 5 : height - 100;
   
@@ -22,6 +33,236 @@ const Homepage = () => {
   };
 
   // 챌린지 관련
+  /**
+    startChallenge	
+    챌린지 시작	
+    /challenges	
+    POST
+  */
+
+  const startChallenge = () => {
+    // const accessToken = sessionStorage.getItem("accessToken")
+    // const host_id = parseInt(sessionStorage.getItem("userIdx"), 10);
+
+    // let guest_id = null;
+    // if (from === "FriendList"){
+    //   guest_id = parseInt(friend.userId, 10);
+    // } else {
+    //   guest_id = parseInt(friend.idx, 10);
+    // }
+
+    // axios.post(`${baseURL}${ChallengeAPI}`, null,
+    // {
+    //   headers: {
+    //     Authorization: `Bearer ${accessToken}`
+    //   }
+    // })
+    // .then((response) => {
+    //     console.log("첼린지 시작");
+    //     showAlert("success", "첼린지 시작");
+    //     setCurStatus(2);
+    // })
+    // .catch((error) => {
+    //     console.error("서버로부터 챌린지 시작 실패", error);
+    //     showAlert("error", "챌린지 시작 실패입니다.");
+    //     console.error(error.code);
+    // });
+  }
+
+  /**
+    createReceipt	
+    지출 내역 영수증 입력	
+    /challenges/receipt/{challenge-id}	
+    POST
+  */
+    const createReceipt = () => {
+      // const accessToken = sessionStorage.getItem("accessToken")
+      // const host_id = parseInt(sessionStorage.getItem("userIdx"), 10);
+  
+      // let guest_id = null;
+      // if (from === "FriendList"){
+      //   guest_id = parseInt(friend.userId, 10);
+      // } else {
+      //   guest_id = parseInt(friend.idx, 10);
+      // }
+  
+      // axios.post(`${baseURL}${ChallengeAPI}/receipt/{challenge-id}`, null,
+      // {
+      //   headers: {
+      //     Authorization: `Bearer ${accessToken}`
+      //   }
+      // })
+      // .then((response) => {
+      //     console.log("지출 내역 영수증 입력");
+      //     showAlert("success", "지출 내역 영수증 입력");
+      //     setCurStatus(2);
+      // })
+      // .catch((error) => {
+      //     console.error("서버로부터 지출 내역 영수증 입력 실패", error);
+      //     showAlert("error", "지출 내역 영수증 입력 실패입니다.");
+      //     console.error(error.code);
+      // });
+    }
+  
+
+  /**
+    createExpense
+    지출 내역 직접 입력
+    /challenges/{challenge-id}
+    POST
+  */
+    const createExpense = () => {
+      // const accessToken = sessionStorage.getItem("accessToken")
+      // const host_id = parseInt(sessionStorage.getItem("userIdx"), 10);
+  
+      // let guest_id = null;
+      // if (from === "FriendList"){
+      //   guest_id = parseInt(friend.userId, 10);
+      // } else {
+      //   guest_id = parseInt(friend.idx, 10);
+      // }
+  
+      // axios.post(`${baseURL}${ChallengeAPI}/{challenge-id}`, null,
+      // {
+      //   headers: {
+      //     Authorization: `Bearer ${accessToken}`
+      //   }
+      // })
+      // .then((response) => {
+      //     console.log("지출 내역 직접 입력");
+      //     showAlert("success", "지출 내역 직접 입력");
+      //     setCurStatus(2);
+      // })
+      // .catch((error) => {
+      //     console.error("서버로부터 지출 내역 직접 입력 실패", error);
+      //     showAlert("error", "지출 내역 직접 입력 실패입니다.");
+      //     console.error(error.code);
+      // });
+    }
+
+
+  /**
+    updateExpense	
+    지출 내역 수정	
+    /challenges	
+    PATCH
+  */
+    const updateExpense = () => {
+      // const accessToken = sessionStorage.getItem("accessToken")
+      // const host_id = parseInt(sessionStorage.getItem("userIdx"), 10);
+  
+      // let guest_id = null;
+      // if (from === "FriendList"){
+      //   guest_id = parseInt(friend.userId, 10);
+      // } else {
+      //   guest_id = parseInt(friend.idx, 10);
+      // }
+  
+      // axios.patch(`${baseURL}${ChallengeAPI}`, "edit",
+      // {
+      //   headers: {
+      //     Authorization: `Bearer ${accessToken}`
+      //   }
+      // })
+      // .then((response) => {
+      //     console.log("지출 내역 수정");
+      //     showAlert("success", "지출 내역 수정");
+      //     setCurStatus(2);
+      // })
+      // .catch((error) => {
+      //     console.error("서버로부터 지출 내역 수정 실패", error);
+      //     showAlert("error", "지출 내역 수정 실패입니다.");
+      //     console.error(error.code);
+      // });
+    }
+
+
+
+  /**
+    deleteExpense	
+    지출 내역 삭제	
+    /challenges/{expense-id}	
+    DELETE
+  */
+  const deleteExpense = () => {
+    // const accessToken = sessionStorage.getItem("accessToken")
+    // const host_id = parseInt(sessionStorage.getItem("userIdx"), 10);
+
+    // let guest_id = null;
+    // if (from === "FriendList"){
+    //     guest_id = parseInt(friend.userId, 10);
+    // } else {
+    //     guest_id = parseInt(friend.idx, 10);
+    // }
+
+    // axios.delete(`${baseURL}${ChallengeAPI}/{expense-id}`,
+    // {
+    //   params: {
+    //     guest_id:guest_id,
+    //     host_id:host_id
+    //   },
+    //   headers: {
+    //     Authorization: `Bearer ${accessToken}`
+    //   }
+    // })
+    // .then((response) => {
+    //   showAlert("success", "지출 내역 삭제 요청 취소 성공입니다.");
+    //   setCurStatus(0);
+    // })
+    // .catch((error) => {
+    //   console.error("서버로부터 지출 내역 삭제 요청 실패", error);
+    //   showAlert("error", "지출 내역 삭제 요청 실패입니다.");
+    //   console.error(error.code);
+    // });
+  }
+
+
+  /**
+    selectDailyChallenge	
+    해당일 챌린지 조회 (일일 잔액, 지출 내역 리스트)	
+    /challenges?date={date}	
+    GET
+  */
+  const selectDailyChallenge = () => {
+    // const accessToken = sessionStorage.getItem("accessToken")
+    // const host_id = parseInt(sessionStorage.getItem("userIdx"), 10);
+    // 
+    // axios.get(`${baseURL}${ChallengeAPI}?date={date}`)
+    //   .then((response) => {
+    //     console.log("해당일 챌린지 조회 (일일 잔액, 지출 내역 리스트)");
+    //     setPostList(response.data);
+    //   })
+    //   .catch((error) => {
+    //     console.error('해당일 챌린지 조회 (일일 잔액, 지출 내역 리스트) 조회 실패', error);
+    //   });
+  };
+
+
+  // 카테고리
+  /**
+    selectCategoryList	
+    카테고리 전체 조회 
+    (default + member’s custom)	/categories	
+    GET
+  */
+  const selectCategoryList = () => {
+    // const accessToken = sessionStorage.getItem("accessToken")
+    // const host_id = parseInt(sessionStorage.getItem("userIdx"), 10);
+    // 
+    // axios.get(`${baseURL}${ChallengeAPI}/list?size=${itemsPerPage}&page=${currentPage}&sort=noticeIdx,desc`)
+    //   .then((response) => {
+    //     console.log("카테고리 전체 조회 성공");
+    //     setPostList(response.data);
+    //   })
+    //   .catch((error) => {
+    //     console.error('카테고리 전체 조회 실패', error);
+    //   });
+  };
+  
+
+  const handleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  }
   
   return (
     <MainStyle>
@@ -106,6 +347,68 @@ const Homepage = () => {
             </svg>
           <div className="bottom_gradation"/>
         </div>
+        <div className="input_purchase">
+          <RegisterButton
+            text="지출 입력"
+            className="white"
+            onClick={handleModal}
+          />
+        </div>
+        {
+          isModalOpen &&
+          <div className="regist_modal">
+            <div>
+              <div>
+                <button onClick={handleModal}>✖</button>
+              </div>
+              <div>
+                <h2 className="regist_title">사용 내역 입력</h2>
+              </div>
+              <div>
+                price 원
+              </div>
+              <div>
+                카테고리
+              </div>
+              <div>
+                <InputText
+                  className="InputText"
+                  type="email"
+                  // alt="Input Email"
+                  placeholder="내용"
+                  value=""
+                  // 추후 email이 들어온다면 sending
+                  onChange={(e) => console.log(e.target.value)}
+                  // onKeyPress={handleKeyPress}
+                />
+              </div>
+              <div>
+                메모
+              </div>
+            </div>
+            <div>
+              <RegisterButton
+                text="OCR"
+                className="white"
+                onClick={handleModal}
+              />
+              <RegisterButton
+                text="확인"
+                onClick={handleModal}
+              />
+              {/* <RegisterButton
+                text="OCR"
+                className="mini_type white"
+                onClick={handleModal}
+              />
+              <RegisterButton
+                text="➕"
+                className="mini_type white"
+                onClick={handleModal}
+              /> */}
+            </div>
+          </div>
+        }
       </div>
     </MainStyle>
   );
