3be194cc4d9a9b108b864c0173948a4a02d4746a
gobro <gobro6366@gmail.com>
2023-09-19 16:10:08 +0900

    [FE/feat] 카카오 oauth 연결 - 각 버튼도 연결 시켜놨습니다.

--- frontend/src/apis/user.ts
+++ frontend/src/apis/user.ts
@@ -62,26 +62,27 @@ export const getAccessToken = async () => {
 
 // 카카오 로그인 리다이렉트
 export const redirectKakao = () => {
-  // const REDIRECT_URI = "http://localhost:3000/login/oauth2/kakao";
-  const REDIRECT_URI = `${SERVER_URL}/login/oauth2/kakao`;
+  const REDIRECT_URI = `${SERVER_URL}/api/oauth/kakao`;
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
   window.location.href = KAKAO_AUTH_URL;
+};
+
+// 카카오 로그인
+export const loginKakao = async (code: string) => {
+  const res = await axiosInstance.get<any>(
+    `/api/oauth/login/kakao?code=${code}`,
+  );
+  return res.data;
 };
 
 // 구글 로그인 리다이렉트
 export const redirectGoogle = () => {
   const CLIENT_ID =
     "124626006679-cq05a5rj2anbrqtfcvv1bjtriqs2pjul.apps.googleusercontent.com";
-  const REDIRECT_URI = `${SERVER_URL}/login/oauth2/google`;
+  const REDIRECT_URI = `${SERVER_URL}/api/oauth/google`;
   const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&service=lso&o2v=1&flowName=GeneralOAuthFlow`;
   // const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=${CLIENT_ID}&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin%2Foauth2%2Fgoogle&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&service=lso&o2v=1&flowName=GeneralOAuthFlow`;
   window.location.href = GOOGLE_AUTH_URL;
-};
-
-// 카카오 로그인
-export const loginKakao = async (code: string) => {
-  const res = await axiosInstance.get<any>(`/user/login/kakao?code=${code}`);
-  return res.data;
 };
 
 // 구글 로그인
