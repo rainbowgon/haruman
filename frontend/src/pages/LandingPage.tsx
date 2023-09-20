4f745b234ea351460fcd4e4c1f7483f1100952d4
gobro <gobro6366@gmail.com>
2023-09-20 17:21:04 +0900

    [FE/feat] landingPage to Homepage setTimeout

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MainStyle from "../components/MainStyle";
 import "../styles/LandingPageStyle.scss";
 import LogoImage from "../assets/logo-mainlogo.svg";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/home");
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
   return (
     <MainStyle>
       <div className="loading">
         <img
-            src={LogoImage}
-            alt="로딩 이미지"
-            className="loadingimg"
-          />
+          src={LogoImage}
+          alt="로딩 이미지"
+          className="loadingimg"
+        />
       </div>
     </MainStyle>
   );
