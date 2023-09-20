39037d4533582a8ce798d3ab68e63d11a46082fc
전혜련 <davidpoplar0531@gmail.com>
2023-09-20 18:20:15 +0900

    [BE/feat] profile 주석 제거

--- backend/src/main/java/ssafy/haruman/domain/challenge/controller/ChallengeController.java
+++ backend/src/main/java/ssafy/haruman/domain/challenge/controller/ChallengeController.java
@@ -26,7 +26,6 @@ import ssafy.haruman.domain.challenge.dto.response.DailyChallengeResponseDto;
 import ssafy.haruman.domain.challenge.dto.response.ExpenseResponseDto;
 import ssafy.haruman.domain.challenge.service.ChallengeService;
 import ssafy.haruman.domain.profile.entity.Profile;
-import ssafy.haruman.domain.profile.service.ProfileServiceImpl;
 import ssafy.haruman.global.response.JsonResponse;
 import ssafy.haruman.global.response.ResponseWrapper;
 
@@ -36,11 +35,10 @@ import ssafy.haruman.global.response.ResponseWrapper;
 public class ChallengeController {
 
     private final ChallengeService challengeService;
-    private final ProfileServiceImpl ps;
 
     @PostMapping
     public ResponseEntity<ResponseWrapper<ChallengeResponseDto>> startChallenge() {
-        Profile profile = ps.findOneProfileById(2L);
+        Profile profile = null;
 
         ChallengeResponseDto responseDto = challengeService.startChallenge(profile);
         return JsonResponse.ok("챌린지가 생성되었습니다.", responseDto);
@@ -74,7 +72,7 @@ public class ChallengeController {
 
     @GetMapping
     public ResponseEntity<ResponseWrapper<DailyChallengeResponseDto>> selectDailyChallenge() {
-        Profile profile = ps.findOneProfileById(2L);
+        Profile profile = null;
 
         DailyChallengeResponseDto responseDto = challengeService.selectDailyChallenge(profile);
         return JsonResponse.ok("챌린지 상세내역을 불러왔습니다.", responseDto);
