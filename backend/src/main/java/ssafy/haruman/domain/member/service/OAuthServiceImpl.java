b48cb0f147c7cf186547c855c541e0b3b5fb509e
이남곤 <nglee.dev@gmail.com>
2023-09-21 16:59:28 +0900

    [BE/feat] 카카오 OAuth, 시큐리티 추가

---
+++ backend/src/main/java/ssafy/haruman/domain/member/service/OAuthServiceImpl.java
@@ -0,0 +1,42 @@
+package ssafy.haruman.domain.member.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import ssafy.haruman.domain.member.entity.Member;
+import ssafy.haruman.domain.member.entity.OAuthServerType;
+import ssafy.haruman.domain.member.repository.MemberRepository;
+import ssafy.haruman.domain.profile.service.ProfileService;
+import ssafy.haruman.global.oauth.authcode.AuthCodeRequestUrlProviderComposite;
+import ssafy.haruman.global.oauth.client.OAuthMemberClientComposite;
+import ssafy.haruman.global.oauth.dto.OAuthResponseDto;
+import ssafy.haruman.global.utils.JwtUtil;
+
+@Service
+@RequiredArgsConstructor
+public class OAuthServiceImpl implements OAuthService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+    private final Long expiredMs = 1000 * 60 * 60L;
+
+    private final MemberRepository memberRepository;
+    private final ProfileService profileService;
+    private final AuthCodeRequestUrlProviderComposite authCodeRequestUrlProviderComposite;
+    private final OAuthMemberClientComposite oauthMemberClientComposite;
+
+    public String getAuthCodeRequestUrl(OAuthServerType oAuthServerType) {
+        return authCodeRequestUrlProviderComposite.provide(oAuthServerType);
+    }
+
+    public String oauthLogin(OAuthServerType oauthServerType, String authCode) {
+        OAuthResponseDto oauthResponseDto = oauthMemberClientComposite.fetch(oauthServerType, authCode);
+        Member savedMember = memberRepository.findByOauthId(oauthResponseDto.getOAuthId())
+                .orElseGet(() -> {
+                    Member newMember = Member.builder().oauthId(oauthResponseDto.getOAuthId()).build();
+                    profileService.saveProfileFromOAuth(newMember, oauthResponseDto.getNickname(), oauthResponseDto.getProfileImageUrl());
+                    return memberRepository.save(newMember);
+                });
+        return JwtUtil.createJwt(savedMember.getId(), secretKey, expiredMs);
+    }
+}
