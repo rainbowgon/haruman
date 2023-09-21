81edb142b09aa1fb979cdef50f1b69b27c8745f8
전혜련 <davidpoplar@naver.com>
2023-09-21 09:49:00 +0900

    [BE/feat] 챌린지 종료 기능 구현

--- backend/src/main/java/ssafy/haruman/domain/challenge/dto/response/DailyChallengeResponseDto.java
+++ backend/src/main/java/ssafy/haruman/domain/challenge/dto/response/DailyChallengeResponseDto.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import ssafy.haruman.domain.challenge.entity.Challenge;
+import ssafy.haruman.domain.challenge.entity.ChallengeStatus;
 
 @Getter
 @Builder
@@ -14,13 +15,17 @@ import ssafy.haruman.domain.challenge.entity.Challenge;
 @AllArgsConstructor
 public class DailyChallengeResponseDto {
 
+    private Integer participantCount;
     private Integer leftoverAmount;
+    private ChallengeStatus challengeStatus;
     private List<ExpenseResponseDto> expenseList;
 
-    public static DailyChallengeResponseDto from(Challenge challenge,
+    public static DailyChallengeResponseDto from(Challenge challenge, Integer participantCount,
             List<ExpenseResponseDto> expenseList) {
         return DailyChallengeResponseDto.builder()
+                .participantCount(participantCount)
                 .leftoverAmount(challenge.getLeftoverAmount())
+                .challengeStatus(challenge.getChallengeStatus())
                 .expenseList(expenseList)
                 .build();
     }
