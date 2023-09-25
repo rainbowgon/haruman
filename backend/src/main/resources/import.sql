bc4032a65000d815cc5f10376bd811212a39f690
최해미 <goal961@naver.com>
2023-09-25 18:08:11 +0900

    [BE/env] 카테고리 초기 데이터

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+//formatter:off
+INSERT INTO category (color, is_default, name) VALUES ('BLACK_01', 'DEFAULT', '기타'), ('YELLOW_01', 'DEFAULT', '식사'), ('BROWN_01', 'DEFAULT', '카페'), ('ORANGE_01', 'DEFAULT', '군것질'), ('RED_01', 'DEFAULT', '패션/뷰티'), ('GREEN_01', 'DEFAULT', '생활'), ('BLUE_01', 'DEFAULT', '건강'), ('PINK_01', 'DEFAULT', '유흥'), ('BLACK_02', 'DEFAULT', '교통'), ('PURPLE_01', 'DEFAULT', '여가');
