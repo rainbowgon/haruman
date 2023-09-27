a1e5045b9e82f459446e19a320674dff0727f346
고세훈 <gobro6366@gmail.com>
2023-09-27 10:59:59 +0900

    [FE/fix] BubbleChart.tsx 스타일 변경 및 페이지 데이터 삽입

--- frontend/src/pages/RankingPage.tsx
+++ frontend/src/pages/RankingPage.tsx
@@ -3,17 +3,19 @@ import BubbleChart from "../components/BubbleChart";
 import CenterContainer from "../components/CenterContainer";
 import MainStyle from "../components/MainStyle";
 import InfoItem from "../components/InfoItem";
+import Register from "../components/RegistButton";
 
 //interface value
 import { User } from "../constants/interfaces";
 
 // styles
 import "../styles/RankinPageStyle.scss";
+import BottomBarSpace from "../components/BottomBarSpace";
 
 const RankingPage = () => {
-  // interface BalanceData {
-  //   balance: number;
-  // }
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const handleFailUser = () => {};
+  const handleNotYetUser = () => {};
 
   interface DataPoint {
     min: number;
@@ -256,13 +258,27 @@ const RankingPage = () => {
   return (
     <CenterContainer>
       <MainStyle>
+        <BottomBarSpace />
         <div className="rankingpage">
           <div className="ranking_header">
             <h3 className="sub_title">[]명이 도전중!</h3>
-            <h1 className="main_title">[]월 []일 챌린지</h1>
+            <h1 className="main_title">
+              {currentDate.getMonth() + 1}월 {currentDate.getDate()}일 챌린지
+            </h1>
           </div>
           <BubbleChart onBubbleClick={handleBubbleClick} />
-
+          <div className="regular_container">
+            <Register
+              text="무지출 챌린지"
+              className="regular brand"
+              onClick={handleNotYetUser}
+            />
+            <Register
+              text="실패한 사람"
+              className="regular white"
+              onClick={handleFailUser}
+            />
+          </div>
           <div className="challengeitems_list">
             {selectedUsers.map((user, index) => (
               <InfoItem
@@ -275,6 +291,7 @@ const RankingPage = () => {
             ))}
           </div>
         </div>
+        <BottomBarSpace />
       </MainStyle>
     </CenterContainer>
   );
