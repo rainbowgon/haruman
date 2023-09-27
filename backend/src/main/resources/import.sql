7586fbe60700b51fd5c82d2bee84935ebd9fda1a
최해미 <goal961@naver.com>
2023-09-27 09:52:44 +0900

    [BE/test] API 테스트를 위한 토큰 생성하는 테스트 코드 구현

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,4 +1,4 @@
 //formatter:off
 INSERT INTO category (color, is_default, name) VALUES ('BLACK_01', 'DEFAULT', '기타'), ('YELLOW_01', 'DEFAULT', '식사'), ('BROWN_01', 'DEFAULT', '카페'), ('ORANGE_01', 'DEFAULT', '군것질'), ('RED_01', 'DEFAULT', '패션/뷰티'), ('GREEN_01', 'DEFAULT', '생활'), ('BLUE_01', 'DEFAULT', '건강'), ('PINK_01', 'DEFAULT', '유흥'), ('BLACK_02', 'DEFAULT', '교통'), ('PURPLE_01', 'DEFAULT', '여가');
-INSERT INTO member (member_id, email, member_name, oauth_server_type, password) VALUES (X'ac2015e990b24b1cb33485df3f06f449', 'superuser@haruman.site', '이남곤', 'DEFAULT', 'leenamgon607');
-INSERT INTO profile (nickname, member_id) VALUES ('남고니', X'ac2015e990b24b1cb33485df3f06f449');
+INSERT INTO member (member_id, email, member_name, oauth_server_type, password) VALUES (X'24ce4681c966419fb739811ebb6f1872', 'superuser@haruman.site', '하루만', 'DEFAULT', 'haruman607');
+INSERT INTO profile (nickname, member_id) VALUES ('남고니', X'24ce4681c966419fb739811ebb6f1872');
