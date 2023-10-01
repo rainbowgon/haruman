f4e36aff5797ad15c4dcebd876b2543cae34fc4c
이남곤 <nglee.dev@gmail.com>
2023-10-01 14:03:29 +0900

    [BE/test] 프로필을 저장하면 연결된 이미지 파일도 같이 저장된다.

--- backend/src/test/java/ssafy/haruman/domain/profile/service/ProfileServiceImplTest.java
+++ backend/src/test/java/ssafy/haruman/domain/profile/service/ProfileServiceImplTest.java
@@ -4,42 +4,48 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import ssafy.haruman.domain.profile.entity.Profile;
+import ssafy.haruman.domain.profile.entity.ProfileImage;
 import ssafy.haruman.domain.profile.repository.ProfileRepository;
 import ssafy.haruman.global.error.exception.ProfileNotFoundException;
 import ssafy.haruman.global.service.S3FileService;
 
-import java.util.Optional;
+import javax.persistence.EntityManager;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
 
+@DataJpaTest
 @DisplayName("프로필 서비스 테스트")
 @ExtendWith(MockitoExtension.class)
 class ProfileServiceImplTest {
 
-    @Mock
+    @Autowired
+    EntityManager entityManager;
+
+    @Autowired
     private ProfileRepository profileRepository;
 
     @Mock
     private S3FileService s3FileService;
 
-    @InjectMocks
     private ProfileServiceImpl profileService;
 
+    private final String S3_PATH = "image/profile/";
 
     @BeforeEach
     void beforeEach() {
+        profileService = new ProfileServiceImpl(profileRepository, s3FileService);
     }
 
     @Test
     @DisplayName("존재하지 않은 ID의 프로필에 접근하면 ProfileNotFoundException이 발생한다.")
     public void noProfileException() throws Exception {
         //given
-        BDDMockito.given(profileRepository.findById(any())).willReturn(Optional.empty());
 
         //when
 
@@ -47,6 +53,26 @@ class ProfileServiceImplTest {
         assertThrows(ProfileNotFoundException.class, () ->
                 profileService.selectOneProfile(1L)
         );
+    }
+
+    @Test
+    @DisplayName("프로필을 저장하면 연결된 이미지 파일도 같이 저장된다.")
+    void saveProfileImage() throws Exception {
+        //given
+        final String TEST_PROFILE_IMAGE = "TEST_PROFILE_IMAGE";
+
+        Profile profile = Profile.builder().
+                nickname("TEST_PROFILE").build();
+        profile.uploadNewProfileImage(S3_PATH, TEST_PROFILE_IMAGE);
+
+        //when
+        profileRepository.save(profile);
+        entityManager.clear();
+
+        //then
+        Profile foundProfile = profileRepository.findById(profile.getId()).orElseThrow();
+        ProfileImage foundProfileImage = foundProfile.getProfileImage();
+        assertThat(foundProfileImage.getSavedFilename()).isEqualTo(TEST_PROFILE_IMAGE);
     }
 
 }
