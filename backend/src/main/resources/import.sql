2b70a24577d8d5fd267fce9598898b7108190b96
이남곤 <nglee.dev@gmail.com>
2023-10-01 19:16:55 +0900

    [BE/test] ProfileService 테스트코드 추가

--- backend/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-//formatter:off
-INSERT INTO category (color, is_default, name) VALUES ('BLACK_01', 'DEFAULT', '기타'), ('YELLOW_01', 'DEFAULT', '식사'), ('BROWN_01', 'DEFAULT', '카페'), ('ORANGE_01', 'DEFAULT', '군것질'), ('RED_01', 'DEFAULT', '패션/뷰티'), ('GREEN_01', 'DEFAULT', '생활'), ('BLUE_01', 'DEFAULT', '건강'), ('PINK_01', 'DEFAULT', '유흥'), ('BLACK_02', 'DEFAULT', '교통'), ('PURPLE_01', 'DEFAULT', '여가');
-INSERT INTO member (member_id, email, member_name, password) VALUES (X'24ce4681c966419fb739811ebb6f1872', 'testuser@haruman.site', '하루만', 'haruman607');
-INSERT INTO profile (nickname, member_id) VALUES ('남고니', X'24ce4681c966419fb739811ebb6f1872');
