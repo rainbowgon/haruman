708ebfe4121df5c0072d86a970c20982eabc58a4
이남곤 <nglee.dev@gmail.com>
2023-10-01 15:01:47 +0900

    [BE/test] 프로필이 삭제되면 연결된 이미지 파일도 같이 삭제된다.

--- backend/src/test/java/ssafy/haruman/domain/profile/service/ProfileServiceImplTest.java
+++ backend/src/test/java/ssafy/haruman/domain/profile/service/ProfileServiceImplTest.java
@@ -1,5 +1,6 @@
 package ssafy.haruman.domain.profile.service;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
 import ssafy.haruman.domain.profile.entity.Profile;
 import ssafy.haruman.domain.profile.entity.ProfileImage;
 import ssafy.haruman.domain.profile.repository.ProfileRepository;
@@ -15,6 +17,9 @@ import ssafy.haruman.global.error.exception.ProfileNotFoundException;
 import ssafy.haruman.global.service.S3FileService;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -40,6 +45,12 @@ class ProfileServiceImplTest {
     @BeforeEach
     void beforeEach() {
         profileService = new ProfileServiceImpl(profileRepository, s3FileService);
+    }
+
+    @AfterEach
+    void afterEach() {
+        entityManager.clear();
+        entityManager.close();
     }
 
     @Test
@@ -75,4 +86,29 @@ class ProfileServiceImplTest {
         assertThat(foundProfileImage.getSavedFilename()).isEqualTo(TEST_PROFILE_IMAGE);
     }
 
+
+    @Test
+    @DisplayName("프로필이 삭제되면 연결된 이미지 파일도 같이 삭제된다.")
+    @Transactional
+    void deleteProfileImage() throws Exception {
+        //given
+        final String TEST_PROFILE_IMAGE = "TEST_PROFILE_IMAGE";
+
+        Profile profile = Profile.builder().
+                nickname("TEST_PROFILE").build();
+        profile.uploadNewProfileImage(S3_PATH, TEST_PROFILE_IMAGE);
+        ProfileImage profileImage = profile.getProfileImage();
+        profileRepository.save(profile);
+        entityManager.flush();
+
+        //when
+        profileRepository.delete(profile);
+        entityManager.flush();
+
+        //then
+        assertThat(profileRepository.findById(profile.getId())).isEqualTo(Optional.empty());
+        TypedQuery<ProfileImage> query = entityManager.createQuery("select pi from ProfileImage pi where pi.id = :profileImageId", ProfileImage.class);
+        List<ProfileImage> foundResultList = query.setParameter("profileImageId", profileImage.getId()).getResultList();
+        assertThat(foundResultList.size()).isEqualTo(0);
+    }
 }
