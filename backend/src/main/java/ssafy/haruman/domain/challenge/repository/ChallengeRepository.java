6a4051a26be9303fdaa05565b1d5aa54975ef38e
chogoal <goal961@naver.com>
2023-10-03 00:40:32 +0900

    [BE/fix] 통계 기능 관련 쿼리 수정

--- backend/src/main/java/ssafy/haruman/domain/challenge/repository/ChallengeRepository.java
+++ backend/src/main/java/ssafy/haruman/domain/challenge/repository/ChallengeRepository.java
@@ -1,10 +1,11 @@
 package ssafy.haruman.domain.challenge.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ssafy.haruman.domain.challenge.entity.Challenge;
+
+import java.util.List;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 
@@ -18,21 +19,21 @@ public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
     List<Challenge> findAllByStatus();
 
     @Query(nativeQuery = true,
-            value = "SELECT p.nickname, f.saved_path, c.start_time, c.used_amount, MAX(e.created_at)\n"
-                    + "FROM challenge c\n"
-                    + "JOIN expense e USING (challenge_id)\n"
-                    + "JOIN profile p USING (profile_id)\n"
-                    + "LEFT JOIN profile_image i USING (profile_id)\n"
-                    + "LEFT JOIN file f ON (f.file_id = i.file_id)\n"
-                    + "WHERE c.challenge_status = 'PROGRESS'\n"
-                    + "GROUP BY e.challenge_id\n"
-                    + "ORDER BY e.created_at, c.start_time")
+            value = "SELECT p.nickname nickname, f.saved_path profileImage, c.start_time startTime, c.used_amount usedAmount, e.created_at createdAt\n" +
+                    "FROM challenge c\n" +
+                    "LEFT JOIN (SELECT challenge_id, MAX(created_at) created_at FROM expense WHERE is_valid = 'VALID' GROUP BY challenge_id) e\n" +
+                    "ON c.challenge_id = e.challenge_id\n" +
+                    "LEFT JOIN profile p ON p.profile_id = c.profile_id\n" +
+                    "LEFT JOIN profile_image i ON p.profile_id = i.profile_id\n" +
+                    "LEFT JOIN file f ON f.file_id = i.file_id\n" +
+                    "WHERE c.challenge_status = 'PROGRESS' AND c.is_valid = 'VALID' AND p.is_valid = 'VALID' AND f.is_valid = 'VALID'\n" +
+                    "ORDER BY e.created_at IS NULL ASC, c.start_time DESC")
     List<ChallengeUserInfoMapping> findChallengeAndExpenseAndProfileByStatus();
 
-    @Query("SELECT SUM(c.leftoverAmount) FROM Challenge c WHERE c.profile = :profileId AND c.challengeStatus = 'SUCCEED'")
-    Integer findAllByProfileAndStatus(Long profileId);
+    @Query("SELECT SUM(c.leftoverAmount) FROM Challenge c WHERE c.profile.id = :profileId AND c.challengeStatus = 'SUCCESS'")
+    Integer sumByProfileAndStatus(Long profileId);
 
-    @Query("SELECT c FROM Challenge c WHERE c.profile = :profileId AND SUBSTR(c.startTime, 1, 7) = :date ORDER BY c.startTime")
+    @Query("SELECT c FROM Challenge c WHERE c.profile.id = :profileId AND SUBSTR(c.startTime, 1, 7) = :date ORDER BY c.startTime")
     List<Challenge> findAllByProfileAndDate(Long profileId, String date);
 
 }
