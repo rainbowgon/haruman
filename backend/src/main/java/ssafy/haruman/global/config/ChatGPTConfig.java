2938af03339a52b49fd22ecf341bed2e82783e3e
이남곤 <nglee.dev@gmail.com>
2023-10-04 23:06:19 +0900

    [BE/refactor] ChatGPT 관련 리팩토링

--- backend/src/main/java/ssafy/haruman/global/config/ChatGPTConfig.java
+++ backend/src/main/java/ssafy/haruman/global/config/ChatGPTConfig.java
@@ -1,11 +1,12 @@
 package ssafy.haruman.global.config;
 
 import com.theokanning.openai.service.OpenAiService;
-import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
 
 /*
  * Chat GPT 라이브러리를 사용하기전, 해당 서비스에 토큰 주입을 하기위한 Config
@@ -15,13 +16,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ChatGPTConfig {
 
-  @Value("${gpt.token}")
-  private String token;
+    @Value("${gpt.token}")
+    private String token;
 
-  @Bean
-  public OpenAiService openAiService() {
-    log.info("token : {}을 활용한 OpenAiService 을 생성합니다.", token);
-    return new OpenAiService(token, Duration.ofSeconds(60));
-  }
+    @Bean
+    public OpenAiService openAiService() {
+        log.info("token : {}을 활용한 OpenAiService 을 생성합니다.", token);
+        return new OpenAiService(token, Duration.ofSeconds(90));
+    }
 
 }
