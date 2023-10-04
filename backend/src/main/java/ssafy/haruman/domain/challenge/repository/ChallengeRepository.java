60c9c4da877b07fa0260ed1909deb15c4b398ea8
chogoal <goal961@naver.com>
2023-10-04 09:38:19 +0900

    [BE/fix] Challenge repository 쿼리 수정

--- backend/src/main/java/ssafy/haruman/domain/challenge/repository/ChallengeRepository.java
+++ backend/src/main/java/ssafy/haruman/domain/challenge/repository/ChallengeRepository.java
@@ -6,14 +6,18 @@ import org.springframework.data.repository.query.Param;
 import ssafy.haruman.domain.challenge.entity.Challenge;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 
-    @Query("select count(*) from Challenge c where c.challengeStatus='PROGRESS'")
+    @Query("SELECT COUNT(c.id) from Challenge c where c.challengeStatus = 'PROGRESS'")
     Integer countByStatus();
 
-    @Query(value = "SELECT * FROM challenge c JOIN profile USING (profile_id) WHERE c.profile_id =:id ORDER BY c.start_time DESC LIMIT 1;", nativeQuery = true)
-    Challenge findFirstChallenge(@Param(value = "id") Long profileId);
+    @Query("SELECT c FROM Challenge c WHERE c.profile.id = :id ORDER BY c.startTime DESC LIMIT 1")
+    Optional<Challenge> findFirstChallenge(@Param(value = "id") Long profileId);
+
+    @Query("SELECT c FROM Challenge c WHERE c.profile.id = :profileId AND c.challengeStatus = 'PROGRESS'")
+    Optional<Challenge> findByProfileAndStatus(Long profileId);
 
     @Query("select c from Challenge c where c.challengeStatus='PROGRESS'")
     List<Challenge> findAllByStatus();
