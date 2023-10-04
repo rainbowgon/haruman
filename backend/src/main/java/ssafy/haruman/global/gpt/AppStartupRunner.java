9a4a4dc56a8cc941ced0f35f36edb0adcc0ea953
이남곤 <nglee.dev@gmail.com>
2023-10-04 16:46:10 +0900

    [BE/fix] 객체 리스트를 활용하여 GPT에 메시지 전송

--- backend/src/main/java/ssafy/haruman/global/gpt/AppStartupRunner.java
+++ backend/src/main/java/ssafy/haruman/global/gpt/AppStartupRunner.java
@@ -1,7 +1,7 @@
-package ssafy.haruman.global.utils;
+package ssafy.haruman.global.gpt;
 
+import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -9,17 +9,21 @@ import org.springframework.util.ResourceUtils;
 import ssafy.haruman.global.gpt.dto.request.GPTCompletionChatRequest;
 import ssafy.haruman.global.gpt.dto.response.CompletionChatResponse;
 import ssafy.haruman.global.gpt.service.GPTChatRestService;
+import ssafy.haruman.global.gpt.vo.BankProduct;
 
-import java.nio.file.Path;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class AppStartupRunner implements ApplicationRunner {
 
-    @Autowired
     private final GPTChatRestService gptChatRestService;
 
-    private final String FILE_NAME = "savings_products.txt";
+    private final String FILE_NAME = "bank_products.json";
 
     public void GPT(String productDescriptions) {
         GPTCompletionChatRequest gptCompletionChatRequest = new GPTCompletionChatRequest(
@@ -30,23 +34,17 @@ public class AppStartupRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
-        Path path = ResourceUtils.getFile("classpath:" + FILE_NAME).toPath();
-        String productDescriptions = FileUtil.readFile(path.toString());
-
-        if (productDescriptions.length() > 2500) {
-            int x = productDescriptions.length() / 2500;
-            for (int i = 0; i < x + 1; i++) {
-                if (i != x) {
-                    String departDescription = productDescriptions.substring(i * 2500, (i * 2500) + 2500);
-                    GPT(departDescription);
-                } else {
-                    String departDescription = productDescriptions.substring(i * 2500, productDescriptions.length());
-                    GPT(departDescription);
-                }
-            }
-        } else {
-            GPT(productDescriptions);
+        List<BankProduct> bankProductList = parseJsonFileToBankProductList();
+        for (BankProduct bankProduct : bankProductList) {
+            GPT(bankProduct.toString());
         }
+    }
+
+    private List<BankProduct> parseJsonFileToBankProductList() throws FileNotFoundException {
+        File file = ResourceUtils.getFile("classpath:" + FILE_NAME);
+        FileReader fileReader = new FileReader(file);
+        Gson gson = new Gson();
+        BankProduct[] array = gson.fromJson(fileReader, BankProduct[].class);
+        return Arrays.asList(array);
     }
 }
