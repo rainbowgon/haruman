f952e2d5972b2bdf1a1794d2ef83380657e100c3
이남곤 <nglee.dev@gmail.com>
2023-10-04 16:42:01 +0900

    [BE/feat] json 파일을 받기 위한 객체 생성

--- backend/src/main/java/ssafy/haruman/global/gpt/vo/BankProduct.java
+++ backend/src/main/java/ssafy/haruman/global/gpt/vo/BankProduct.java
@@ -1,2 +1,21 @@
-package ssafy.haruman.global.gpt.vo;public class BankProduct {
+package ssafy.haruman.global.gpt.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankProduct {
+
+    private String bank;
+    private String name;
+    private String description;
+    private Double interest_rate;
+
+    @Override
+    public String toString() {
+        return "bank=" + bank + ", name=" + name + ", description=" + description + ", interest_rate=" + interest_rate;
+    }
 }
