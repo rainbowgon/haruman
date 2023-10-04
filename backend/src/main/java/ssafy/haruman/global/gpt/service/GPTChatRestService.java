85d9b7357b4ea0c40800326fdbe4e356d41b9784
이남곤 <nglee.dev@gmail.com>
2023-10-04 18:38:38 +0900

    [BE/feat] GPT 소요 시간 측정

--- backend/src/main/java/ssafy/haruman/global/gpt/service/GPTChatRestService.java
+++ backend/src/main/java/ssafy/haruman/global/gpt/service/GPTChatRestService.java
@@ -4,6 +4,7 @@ import com.theokanning.openai.completion.CompletionResult;
 import com.theokanning.openai.completion.chat.ChatCompletionResult;
 import com.theokanning.openai.service.OpenAiService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ssafy.haruman.global.gpt.dto.request.GPTCompletionChatRequest;
@@ -11,6 +12,7 @@ import ssafy.haruman.global.gpt.dto.request.GPTCompletionRequest;
 import ssafy.haruman.global.gpt.dto.response.CompletionChatResponse;
 import ssafy.haruman.global.gpt.dto.response.CompletionResponse;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class GPTChatRestService {
@@ -32,9 +34,15 @@ public class GPTChatRestService {
 
     @Transactional
     public CompletionChatResponse GPT(String productDescriptions) {
+        long beforeTime = System.currentTimeMillis();
+
         GPTCompletionChatRequest gptCompletionChatRequest = new GPTCompletionChatRequest(
                 "gpt-3.5-turbo", "user", productDescriptions, 1000);
         CompletionChatResponse completionChatResponse = completionChat(gptCompletionChatRequest);
+
+        long afterTime = System.currentTimeMillis();
+        long secDiffTime = (afterTime - beforeTime) / 1000;
+        log.infogi t("GPT 응답 시간 [{} sec] 소요", secDiffTime);
         return completionChatResponse;
     }
 }
