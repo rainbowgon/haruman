0a50473f27a580057f49a611e4bc32d8d23510dd
이남곤 <nglee.dev@gmail.com>
2023-10-04 18:06:22 +0900

    [BE/fix] Duration 설정

--- backend/src/main/java/ssafy/haruman/global/gpt/service/GPTChatRestService.java
+++ backend/src/main/java/ssafy/haruman/global/gpt/service/GPTChatRestService.java
@@ -3,37 +3,35 @@ package ssafy.haruman.global.gpt.service;
 import com.theokanning.openai.completion.CompletionResult;
 import com.theokanning.openai.completion.chat.ChatCompletionResult;
 import com.theokanning.openai.service.OpenAiService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ssafy.haruman.global.config.ChatGPTConfig;
 import ssafy.haruman.global.gpt.dto.request.GPTCompletionChatRequest;
 import ssafy.haruman.global.gpt.dto.request.GPTCompletionRequest;
 import ssafy.haruman.global.gpt.dto.response.CompletionChatResponse;
 import ssafy.haruman.global.gpt.dto.response.CompletionResponse;
 
+import java.time.Duration;
+
 @Service
-@RequiredArgsConstructor
 public class GPTChatRestService {
 
-    private final OpenAiService openAiService;
+    private OpenAiService openAiService;
+
+    public GPTChatRestService(ChatGPTConfig chatGPTConfig) {
+        this.openAiService = new OpenAiService(chatGPTConfig.getToken(), Duration.ofSeconds(120));
+    }
 
     @Transactional
     public CompletionResponse completion(final GPTCompletionRequest restRequest) {
         CompletionResult result = openAiService.createCompletion(GPTCompletionRequest.of(restRequest));
-        CompletionResponse response = CompletionResponse.of(result);
-
-
-        return response;
+        return CompletionResponse.of(result);
     }
 
     @Transactional
     public CompletionChatResponse completionChat(GPTCompletionChatRequest gptCompletionChatRequest) {
         ChatCompletionResult chatCompletion = openAiService.createChatCompletion(
                 GPTCompletionChatRequest.of(gptCompletionChatRequest));
-
-        CompletionChatResponse response = CompletionChatResponse.of(chatCompletion);
-
-
-        return response;
+        return CompletionChatResponse.of(chatCompletion);
     }
 }
