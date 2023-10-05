3331efe36f81eb293f19805e08023ee60c16b6b8
이정명 <leejm9704@naver.com>
2023-10-05 23:13:53 +0900

    [FE/feat] save 페이지 추천 링크 연결 및 캘린더 페이지 에러 처리

--- frontend/src/components/Save/SavepageItem.tsx
+++ frontend/src/components/Save/SavepageItem.tsx
@@ -6,7 +6,10 @@ import Shinhan from "../../assets/image/bank/image_shinhan.svg"
 import Kb from "../../assets/image/bank/image_kb.svg"
 import Hana from "../../assets/image/bank/image_hana.svg"
 import Woori from "../../assets/image/bank/image_woori.svg"
+import Kakao from "../../assets/image/bank/image_kakao.svg"
+import Toss from "../../assets/image/bank/image_toss.svg" 
 import Question from "../../assets/icons/consumptions/icon-question.svg"
+import { useNavigate } from "react-router-dom"
 
 export interface SavepageItemProps {
   imgName? : string;
@@ -28,7 +31,29 @@ const SavepageItem = ({
 
   // 페이지 확장 시 사용할 기능
   const handleItemComponent = () => {
-
+    if (imgName){
+      if (imgName === "신한은행"){
+        window.location.href = 'https://m.shinhan.com/rib/mnew/index.jsp#220011110001';
+        return "shinhan";
+      } else if(imgName === "국민은행") {
+        window.location.href = 'https://obank.kbstar.com/quics?page=C016528#loading';
+        return "kb";
+      } else if(imgName === "하나은행") {
+        window.location.href = 'https://www.kebhana.com/cont/mall/mall09/mall0902/mall090201/index.jsp';
+        return "hana";
+      } else if(imgName === "우리은행") {
+        window.location.href = 'https://spot.wooribank.com/pot/Dream?withyou=PODEP0021';
+        return "woori";
+      } else if(imgName === "카카오뱅크") {
+        window.location.href = 'https://www.kakaobank.com/products/savings';
+        return "kakao";
+      } else if(imgName === "토스뱅크") {
+        window.location.href = 'https://www.tossbank.com/product-service/savings/savings-gulbi';
+        return "toss";
+      } else {
+  
+      }
+    }
   }
 
   const handleName = (img : string) => {
@@ -39,7 +64,11 @@ const SavepageItem = ({
     } else if(img === "하나은행") {
         return "hana";
     } else if(img === "우리은행") {
-        return "woori";
+      return "woori";
+    } else if(img === "카카오뱅크") {
+      return "kakao";
+    } else if(img === "토스뱅크") {
+      return "toss";
     } else {
 
     }
@@ -55,6 +84,10 @@ const SavepageItem = ({
         return Hana;
     } else if(img === "우리은행") {
         return Woori;
+    } else if(img === "카카오뱅크") {
+        return Kakao;
+    } else if(img === "토스뱅크") {
+        return Toss;
     } else {
         return Question;
     }
@@ -65,7 +98,7 @@ const SavepageItem = ({
     <div className="save_item_component" onClick={handleItemComponent}>
       <div className="save_item_img_circle">
         {
-          <img className={`save_item_img ${imgName && handleName(imgName)}`} src={handleImage(imgName)} alt="아이콘"/>
+          <img className={`save_item_img`} src={handleImage(imgName)} alt="아이콘"/>
         }
       </div>
       <div className="save_item_contents">
