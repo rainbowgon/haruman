300a0ac6146f571d2fe4c42638dcf986469657e8
이남곤 <nglee.dev@gmail.com>
2023-10-06 01:02:10 +0900

    [BE/fix] 더미 프로필의 url 수정

---
+++ backend/src/main/java/ssafy/haruman/global/utils/UrlUtil.java
@@ -0,0 +1,32 @@
+package ssafy.haruman.global.utils;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class UrlUtil {
+
+    public static String getFinalURL(String urlPath) {
+        try {
+            URL url = new URL(urlPath);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setInstanceFollowRedirects(false);
+            con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36");
+            con.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
+            con.addRequestProperty("Referer", "https://www.google.com/");
+            con.connect();
+            // Header에서 Status Code를 뽑는다.
+            int resCode = con.getResponseCode();
+            // http코드가 301(영구이동), 302(임시 이동), 303(기타 위치 보기) 이면 또다시 이 함수를 태운다. 재귀함수.
+            if (resCode == HttpURLConnection.HTTP_SEE_OTHER || resCode == HttpURLConnection.HTTP_MOVED_PERM || resCode == HttpURLConnection.HTTP_MOVED_TEMP) {
+                String Location = con.getHeaderField("Location");
+                if (Location.startsWith("/")) {
+                    Location = url.getProtocol() + "://" + url.getHost() + Location;
+                }
+                return getFinalURL(Location);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return urlPath;
+    }
+}
